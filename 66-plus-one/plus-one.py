fbb9f0a8688fe2020961a8f0cde54c491e96d6ff
Rutvi Sharma <soberhomer67@gmail.com>
2026-07-30 20:05:01 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (54.28%) - LeetSync

---
+++ 66-plus-one/plus-one.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def plusOne(self, digits):
+        for i in range(len(digits) - 1, -1, -1):
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+            digits[i] = 0
+        # If we reach here, all digits were 9
+        return [1] + digits
