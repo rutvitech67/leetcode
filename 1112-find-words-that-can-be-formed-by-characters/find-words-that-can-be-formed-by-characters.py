9f01f5e48f406f632c7a116fa1712167716ca59a
Rutvi Sharma <soberhomer67@gmail.com>
2026-08-16 11:39:16 +0530

    Time: 263 ms (19.47%) | Memory: 13 MB (16.22%) - LeetSync

--- 1112-find-words-that-can-be-formed-by-characters/find-words-that-can-be-formed-by-characters.py
+++ 1112-find-words-that-can-be-formed-by-characters/find-words-that-can-be-formed-by-characters.py
@@ -3,10 +3,10 @@ class Solution(object):
     def countCharacters(self, words, chars):
         char_count = Counter(chars)
         total_length = 0
-
         for word in words:
             word_count = Counter(word)
-            if all(word_count[c] <= char_count[c] for c in word_count):
+            if all(word_count[c] <= char_count
+            [c] for c in word_count):
                 total_length += len(word)
 
         return total_length
