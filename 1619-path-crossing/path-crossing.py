7b1583c1c7dc23f8a6b6767201d4ae9af5904bd9
Rutvi Sharma <soberhomer67@gmail.com>
2026-08-25 18:23:21 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (45.26%) - LeetSync

---
+++ 1619-path-crossing/path-crossing.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def isPathCrossing(self, path):
+        x, y = 0, 0
+        visited = {(0,0)}
+        for move in path:
+            if move == 'N':
+                y += 1
+            elif move == 'S':
+                y -= 1
+            elif move == 'E':
+                x += 1
+            elif move == 'W':
+                x -= 1
+            
+            if (x,y) in visited:
+                return True
+            visited.add((x,y))
+        
+        return False
