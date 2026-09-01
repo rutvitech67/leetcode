0f1d8316ebad24204f6bace78c9533bd60f920a8
Rutvi Sharma <soberhomer67@gmail.com>
2026-09-01 10:08:41 +0530

    Time: 31 ms (46.67%) | Memory: 12.9 MB (75.56%) - LeetSync

---
+++ 1775-design-an-ordered-stream/design-an-ordered-stream.py
@@ -0,0 +1,15 @@
+class OrderedStream(object):
+
+    def __init__(self, n):
+        self.stream = [None] * n
+        self.ptr = 0  
+
+    def insert(self, idKey, value):
+        self.stream[idKey - 1] = value
+        result = []
+
+        while self.ptr < len(self.stream) and self.stream[self.ptr] is not None:
+            result.append(self.stream[self.ptr])
+            self.ptr += 1
+
+        return result
