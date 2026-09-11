b88e18bcccb63d3d50d1ce486d1b791bb6781dca
Rutvi Sharma <soberhomer67@gmail.com>
2026-09-11 19:26:38 +0530

    Time: 228 ms (66.67%) | Memory: 22.4 MB (14.81%) - LeetSync

---
+++ 2138-sum-of-beauty-in-the-array/sum-of-beauty-in-the-array.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def sumOfBeauties(self, nums):
+        n = len(nums)
+
+        left_max = [0] * n
+        right_min = [0] * n
+
+        left_max[0] = nums[0]
+        for i in range(1, n):
+            left_max[i] = max(left_max[i - 1], nums[i])
+
+        right_min[n - 1] = nums[n - 1]
+        for i in range(n - 2, -1, -1):
+            right_min[i] = min(right_min[i + 1], nums[i])
+
+        beauty = 0
+
+        for i in range(1, n - 1):
+            if left_max[i - 1] < nums[i] < right_min[i + 1]:
+                beauty += 2
+            elif nums[i - 1] < nums[i] < nums[i + 1]:
+                beauty += 1
+
+        return beauty
