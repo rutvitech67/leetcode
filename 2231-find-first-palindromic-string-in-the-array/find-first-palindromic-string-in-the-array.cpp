2f3699de6d2361640fc2a639660992be19b988cc
Rutvi Sharma <soberhomer67@gmail.com>
2026-09-17 08:16:19 +0530

    Time: 16 ms (6.49%) | Memory: 30 MB (5.48%) - LeetSync

---
+++ 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string firstPalindrome(vector<string>& words) {
+        for (string word : words) {
+            string rev = word;
+            reverse(rev.begin(), rev.end());
+
+            if (word == rev) {
+                return word;
+            }
+        }
+        return "";
+    }
+};
